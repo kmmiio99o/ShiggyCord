2c2508bcae74dddf3100cd20c52e42dfda73cbaf
cocobo1 <CocoboTheOne@gmail.com>
2025-10-11 18:06:51 +0300

    Revert "port messagefix from ShiggyCord"
    This reverts commit beb52729f59de9251e2bc49b7551eff7bd33fe83.

--- src/core/plugins/index.ts
+++ src/core/plugins/index.ts
@@ -1,25 +1,22 @@
 import { PluginInstanceInternal } from "@lib/addons/plugins/types";
 
 interface CorePlugin {
-  default: PluginInstanceInternal;
-  preenabled: boolean;
+    default: PluginInstanceInternal;
+    preenabled: boolean;
 }
 
 // Called from @lib/plugins
 export const getCorePlugins = (): Record<string, CorePlugin> => ({
-  "bunny.quickinstall": require("./quickinstall"),
-  "bunny.badges": require("./badges"),
-  "bunny.notrack": { ...require("./notrack"), preenabled: true },
-  "bunny.messagefix": { default: require("./messagefix"), preenabled: true },
+    "bunny.quickinstall": require("./quickinstall"),
+    "bunny.badges": require("./badges"),
+    "vendetta.notrack": require("./notrack")
 });
 
 /**
  * @internal
  */
-export function defineCorePlugin(
-  instance: PluginInstanceInternal,
-): PluginInstanceInternal {
-  // @ts-expect-error
-  instance[Symbol.for("bunny.core.plugin")] = true;
-  return instance;
+export function defineCorePlugin(instance: PluginInstanceInternal): PluginInstanceInternal {
+    // @ts-expect-error
+    instance[Symbol.for("bunny.core.plugin")] = true;
+    return instance;
 }
