7a4a8ec23e82b30f6b56685421ed7e24fa5d7b3b
cocobo1 <CocoboTheOne@gmail.com>
2025-10-11 18:06:31 +0300

    Revert "port information menu look from ShiggyCord (was exclusive only for core"
    This reverts commit 93d5c354a119be52a3a8d99bcc49860c737e6b3f.

--- src/core/ui/settings/pages/Plugins/models/vendetta.ts
+++ src/core/ui/settings/pages/Plugins/models/vendetta.ts
@@ -3,38 +3,32 @@ import { useProxy } from "@core/vendetta/storage";
 
 import { UnifiedPluginModel } from ".";
 
-export default function unifyVdPlugin(
-  vdPlugin: VendettaPlugin,
-): UnifiedPluginModel {
-  return {
-    id: vdPlugin.id,
-    name: vdPlugin.manifest.name,
-    description: vdPlugin.manifest.description,
-    authors: vdPlugin.manifest.authors,
-    icon: vdPlugin.manifest.vendetta?.icon,
+export default function unifyVdPlugin(vdPlugin: VendettaPlugin): UnifiedPluginModel {
+    return {
+        id: vdPlugin.id,
+        name: vdPlugin.manifest.name,
+        description: vdPlugin.manifest.description,
+        authors: vdPlugin.manifest.authors,
+        icon: vdPlugin.manifest.vendetta?.icon,
 
-    getBadges() {
-      return [];
-    },
-    isEnabled: () => vdPlugin.enabled,
-    isInstalled: () =>
-      Boolean(vdPlugin && VdPluginManager.plugins[vdPlugin.id]),
-    usePluginState() {
-      useProxy(VdPluginManager.plugins[vdPlugin.id]);
-    },
-    toggle(start: boolean) {
-      start
-        ? VdPluginManager.startPlugin(vdPlugin.id)
-        : VdPluginManager.stopPlugin(vdPlugin.id);
-    },
-    resolveSheetComponent() {
-      // Return a Promise resolving to the unified PluginInfoActionSheet for consistency
-      return Promise.resolve({
-        default: require("../sheets/PluginInfoActionSheet").default,
-      });
-    },
-    getPluginSettingsComponent() {
-      return VdPluginManager.getSettings(vdPlugin.id);
-    },
-  };
+        getBadges() {
+            return [];
+        },
+        isEnabled: () => vdPlugin.enabled,
+        isInstalled: () => Boolean(vdPlugin && VdPluginManager.plugins[vdPlugin.id]),
+        usePluginState() {
+            useProxy(VdPluginManager.plugins[vdPlugin.id]);
+        },
+        toggle(start: boolean) {
+            start
+                ? VdPluginManager.startPlugin(vdPlugin.id)
+                : VdPluginManager.stopPlugin(vdPlugin.id);
+        },
+        resolveSheetComponent() {
+            return import("../sheets/VdPluginInfoActionSheet");
+        },
+        getPluginSettingsComponent() {
+            return VdPluginManager.getSettings(vdPlugin.id);
+        },
+    };
 }
