6cb7f99b608eea874c2db8c71ee0ec37066ec728
cocobo1 <CocoboTheOne@gmail.com>
2025-10-11 17:22:52 +0300

    version(bump): bumped to v1,3,1

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -74,7 +74,7 @@ const config = {
                                 constModules: {
                                     globals: {
                                         "bunny-build-info": {
-                                            version: `"1.3.0"`
+                                            version: `"1.3.1"`
                                         }
                                     }
                                 },
