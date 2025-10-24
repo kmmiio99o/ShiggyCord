9e8456c0fe95824808ad00ec025fe58c3e04eb8f
kmmiio99o <164557248+kmmiio99o@users.noreply.github.com>
2025-10-24 22:38:34 +0200

    Bump bunny-build-info version to 1.1.6

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -77,7 +77,7 @@ const config = {
                 constModules: {
                   globals: {
                     "bunny-build-info": {
-                      version: `"1.1.5-4"`,
+                      version: `"1.1.6"`,
                     },
                   },
                 },
