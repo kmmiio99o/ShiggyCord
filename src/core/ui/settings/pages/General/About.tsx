00db6c11f28950b20d6008ed7ca84dd24b6929c7
kmmiio99o <szymonladziak00@gmail.com>
2026-01-13 20:58:31 +0100

    Jolly -> Shiggy

--- src/core/ui/settings/pages/General/About.tsx
+++ src/core/ui/settings/pages/General/About.tsx
@@ -97,7 +97,7 @@ export default function About() {
             variant="text-md/medium"
             style={{ textAlign: "center", marginTop: 4 }}
           >
-            Jolly your discord client!
+            Shiggy your discord client!
           </Text>
         </View>
 
