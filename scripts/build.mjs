5bdc55a8b055f5557a116f3d77daa97c2836b424
kmmiio99o <szymonladziak00@gmail.com>
2026-02-06 19:33:37 +0100

    feat: bump to 1.4.1.1

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -75,7 +75,7 @@ const config = {
                 constModules: {
                   globals: {
                     "bunny-build-info": {
-                      version: `"1.4.1"`,
+                      version: `"1.4.1.1"`,
                     },
                   },
                 },
